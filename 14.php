bb7cea9f21bd42c954c94bf8484f336c465710bf
takumi <takumik9823@gmail.com>
2021-09-17 23:54:39 +0900

    [15]数列の検索

--- 14.php
+++ 14.php
@@ -35,3 +35,15 @@ for ($i = 0; $i < $n; $i++) {
     [$user, $blood] = explode(' ', trim(fgets(STDIN)));
     $userToBlood[$user] = $blood;
 }
+
+$m = trim(fgets(STDIN));
+$bloodToFortune = [];
+
+for ($i = 0; $i < $m; $i++) {
+    [$blood, $fortune] = explode(' ', trim(fgets(STDIN)));
+    $bloodToFortune[$blood] = $fortune;
+}
+
+foreach ($userToBlood as $user => $blood) {
+    echo $user . ' ' . $bloodToFortune[$blood] . "\n";
+}
