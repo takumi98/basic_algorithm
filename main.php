4b08fda0eb8a567d5118be2626e2d81b9190ab0d
takumi <takumik9823@gmail.com>
2021-10-14 23:35:56 +0900

    総当り計算

--- main.php
+++ main.php
@@ -37,3 +37,47 @@ for($i = 1; $i <= 22; $i++){
 echo('<pre>');
 var_dump($hoge);
 echo('<pre>');
+
+// 座席をナンバーに変換
+function seatConversion($seat,$num){
+  while ($seat_num = current($seat)) {
+      if ($seat_num == $num) {
+          $num = key($seat);
+          return $num;
+      }
+      next($seat);
+  }
+}
+
+// 席順のコスト計算
+function costCalculation($num1,$num2){
+  // 入力された席のコストを計算
+  //　コストを計算する席
+  // $num1 = $a;
+  // $num2 = $b;
+  // これを全通りやる
+  // 1,2 = 0
+  if ($num1 == 1 && $num2 == 2 || $num1 == 2 && $num2 == 1){
+    $cost = 0;
+  }
+  // 21,22 = 0
+    elseif ($num1 == 21 && $num2 == 22 || $num1 == 22 && $num2 == 21){
+      $cost = 0;
+    }
+    // 他のテーブル同士
+    else {
+      $cost = 5;
+    }
+  return $cost;
+}
+
+$result = 0;
+for ($i = 1; $i <= 1000; $i++){
+  $seat_num1 = $n[$i][0];
+  $seat_num2 = $n[$i][1];
+  $num1 = seatConversion($seat,$seat_num1);
+  $num2 = seatConversion($seat,$seat_num2);
+  $temp = costCalculation($num1,$num2);
+  $result = $result + $temp;
+  $temp = 0;
+};
